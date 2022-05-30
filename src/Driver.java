ebf9274ccf991739bb38ed7e780f951d2d15ba92
TheCaister <edisoncai2001@gmail.com>
2022-05-30 12:51:24 +0200

    Getting started with driver and huffman coding base files

---
+++ src/Driver.java
@@ -0,0 +1,25 @@
+public class Driver {
+    enum CompressionType{
+        HUFFMAN,
+        DCT,
+        BLOCK_MOTION
+    }
+
+    public static void runCompression(CompressionType type){
+        switch(type){
+            case DCT -> System.out.println("Running the Discrete Cosine Transformation...");
+            case HUFFMAN -> System.out.println("Running Huffman Coding...");
+            case BLOCK_MOTION -> System.out.println("Running Block Motion compression...");
+
+            default -> {
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CompressionType type = CompressionType.DCT;
+
+        runCompression(type);
+    }
+}
